3bbd64193de6e825cb0b50f5a2a0186ad6121efd
steve <sylvius4@gmail.com>
2023-11-02 14:01:44 +0900

    chore: set redis url by env

--- apps/socket/src/main.ts
+++ apps/socket/src/main.ts
@@ -1,10 +1,12 @@
+import { EnvService } from '@app/common/env/env.service';
 import { NestFactory } from '@nestjs/core';
 import { RedisIoAdapter } from './redis-io.adapter';
 import { SocketModule } from './socket.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(SocketModule);
-  const redisIoAdapter = new RedisIoAdapter(app);
+  const envService = new EnvService();
+  const redisIoAdapter = new RedisIoAdapter(app, envService);
   redisIoAdapter.connectToRedis();
   app.useWebSocketAdapter(redisIoAdapter);
 
--- apps/socket/src/redis-io.adapter.ts
+++ apps/socket/src/redis-io.adapter.ts
@@ -2,12 +2,21 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions, Socket, Server } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
+import { INestApplicationContext } from '@nestjs/common';
+import { EnvService } from '@app/common/env/env.service';
 
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
   private static socketIo: Server;
   private static pubClient;
   private static subClient;
+
+  constructor(
+    private readonly app: INestApplicationContext,
+    private readonly envService: EnvService
+  ) {
+    super(app);
+  }
 
   private ioHandler(io: Server) {
     const pubClient = RedisIoAdapter.pubClient;
@@ -35,7 +44,11 @@ export class RedisIoAdapter extends IoAdapter {
   }
 
   async connectToRedis(): Promise<void> {
-    RedisIoAdapter.pubClient = createClient({ url: `redis://localhost:6379` });
+    RedisIoAdapter.pubClient = createClient({
+      url: `redis://${this.envService.get('REDIS_HOST')}:${this.envService.get(
+        'REDIS_PORT'
+      )}`,
+    });
     RedisIoAdapter.subClient = RedisIoAdapter.pubClient.duplicate();
 
     await Promise.all([
