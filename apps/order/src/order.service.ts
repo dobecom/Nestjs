79a33d69ad3316bd4a86bfcd7bbd8becbfcf3211
steve <sylvius4@gmail.com>
2023-12-12 17:36:59 +0900

    add: order transaction

--- apps/order/src/order.service.ts
+++ apps/order/src/order.service.ts
@@ -1,13 +1,16 @@
 import { OrderEntity } from '@app/db/entities/order.entity';
+import { PaymentEntity } from '@app/db/entities/payment.entity';
+import { UserEntity } from '@app/db/entities/user.entity';
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
 import { lastValueFrom } from 'rxjs';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class OrderService {
   constructor(
+    private readonly dataSource: DataSource,
     @InjectRepository(OrderEntity)
     private readonly orderRepository: Repository<OrderEntity>,
     @Inject('PAYMENT_SERVICE')
@@ -15,25 +18,34 @@ export class OrderService {
   ) {}
 
   async createOrder(data) {
+    const queryRunner = await this.dataSource.createQueryRunner();
+    const { userId, orderList } = data;
     try {
-      const { userId, orderList } = data;
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+
       const paymentRequest = await this.paymentCp.send('payment-create', {
         userId,
         orderList,
       });
       const paymentResult = await lastValueFrom(paymentRequest);
-
+      
       const orderResult = [];
       for (const order of orderList) {
         const { title, description } = order;
-        const saveResult = await this.orderRepository.save({
-          title,
-          description,
-          user: {
-            id: userId,
-          },
-          payment: paymentResult.id,
-        });
+
+        const orderEntity = new OrderEntity();
+        const userEntity = new UserEntity();
+        userEntity.id = userId;
+        const paymentEntity = new PaymentEntity();
+        paymentEntity.id = paymentResult.id;
+        orderEntity.title = title;
+        orderEntity.description = description;
+        orderEntity.user = userEntity;
+        orderEntity.payment = paymentEntity;
+
+        const saveResult = await queryRunner.manager.save(orderEntity);
+        
         if (saveResult) {
           orderResult.push({
             title,
@@ -41,13 +53,16 @@ export class OrderService {
           });
         }
       }
-
+      await queryRunner.commitTransaction();
       return {
         orders: orderResult,
         payment: paymentResult,
       };
     } catch (err) {
+      await queryRunner.rollbackTransaction();
       throw new RpcException(err);
+    } finally {
+      await queryRunner.release();
     }
   }
 }
--- apps/payment/src/payment.service.ts
+++ apps/payment/src/payment.service.ts
@@ -1,5 +1,6 @@
 import { PaymentEntity } from '@app/db/entities/payment.entity';
 import { Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -12,20 +13,24 @@ export class PaymentService {
 
   async createPayment(data) {
     const { userId, orderList } = data;
-    const description =
-      orderList
-        .map((order, index) => {
-          if (index < 3) return order.title;
-        })
-        .join(', ') + `등 ${orderList.length}개의 주문`;
+    try {
+      const description =
+        orderList
+          .map((order, index) => {
+            if (index < 3) return order.title;
+          })
+          .join(', ') + `등 ${orderList.length}개의 주문`;
 
-    const result = await this.paymentRepository.save({
-      title: `${new Date().getTime().toString()}_${userId}`,
-      description,
-      user: {
-        id: userId,
-      },
-    });
-    return result;
+      const result = await this.paymentRepository.save({
+        title: `${new Date().getTime().toString()}_${userId}`,
+        description,
+        user: {
+          id: userId,
+        },
+      });
+      return result;
+    } catch (err) {
+      throw new RpcException(err);
+    }
   }
 }
