83a6a7a124be639fab9e1580af50c258c96ac1cc
steve <sylvius4@gmail.com>
2024-07-25 10:44:48 +0900

    del: move webhook to DevOps repo

--- apps/webhook-agent/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { WebhookAgentModule } from './webhook-agent.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(WebhookAgentModule);
-  await app.listen(3000);
-}
-bootstrap();
