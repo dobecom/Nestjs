bc072d9b601a3ba13c0d15c0628669fa1ce91266
steve <sylvius4@gmail.com>
2024-08-16 15:59:40 +0900

    mod: redis pipeline - redis>ioredis

--- libs/redis/src/redis.service.ts
+++ libs/redis/src/redis.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { createClient, SetOptions } from 'redis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
-  private readonly redis = createClient({
-    url: `redis://${this.config.get('REDIS_HOST') || 'localhost'}:${
-      this.config.get('REDIS_PORT') || 6379
-    }`,
-  });
+  private readonly redis: Redis;
 
   constructor(private readonly config: ConfigService) {
-    this.redis.connect();
+    this.redis = new Redis({
+      host: this.config.get('REDIS_HOST') || 'localhost',
+      port: this.config.get('REDIS_PORT') || 6379,
+      password: this.config.get('REDIS_PASSWORD'),
+    });
   }
 
   async get(key: string): Promise<string> {
@@ -19,12 +19,20 @@ export class RedisService {
   }
 
   async set(key: string, value: string, expire?: number) {
-    return this.redis.set(key, value, {
-      EX: expire ?? (this.config.get('REDIS_TTL') || 10),
-    } as SetOptions);
+    return this.redis.set(
+      key,
+      value,
+      'EX',
+      expire ?? (this.config.get('REDIS_TTL') || 10)
+    );
   }
 
   async del(key: string): Promise<number> {
     return this.redis.del(key);
+  }
+
+  async pipeline() {
+    const pipeline = this.redis.pipeline();
+    return pipeline;
   }
 }
